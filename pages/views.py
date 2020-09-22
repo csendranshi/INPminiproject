ff9b5692652878873d2f56b6a08f10337f4956e5
reuben21 <reuben211999@icloud.com>
2020-09-22 17:57:19 +0530

    API Added

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "index.html", {})
+    top_stories = "https://newsapi.org/v2/top-headlines?country=in&apiKey=95c4ed43f7644dc8a05175665cd04b7a"
+    # 95c4ed43f7644dc8a05175665cd04b7a - api key
+    r = requests.get(top_stories).json()
+    top = []
+    print(r['articles'])
+    for i in r['articles']:
+        top_story = {
+            'image': i['urlToImage'],
+            'title': i['title']
+        }
+        top.append(top_story)
+    context_of_top_stories = {'top_stories': top}
+    return render(request, "index.html", context_of_top_stories)
 
 
 def auth_view(request, *args, **kwargs):
