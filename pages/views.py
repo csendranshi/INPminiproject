ff010cf1fc53c7cb729d0fab8e4d5eae9e9d7f00
reuben21 <reuben211999@icloud.com>
2020-09-24 19:52:17 +0530

    Updated Code

--- pages/views.py
+++ pages/views.py
@@ -6,18 +6,19 @@ import requests
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    top_stories = "https://newsapi.org/v2/top-headlines?country=in&apiKey=95c4ed43f7644dc8a05175665cd04b7a"
-    # 95c4ed43f7644dc8a05175665cd04b7a - api key
-    r = requests.get(top_stories).json()
-    top = []
-    for i in r['articles']:
-        top_story = {
-            'image': i['urlToImage'],
-            'title': i['title']
-        }
-        top.append(top_story)
-    context_of_top_stories = {'top_stories': top}
-    return render(request, "index.html", context_of_top_stories)
+    # top_stories = "https://newsapi.org/v2/top-headlines?country=in&apiKey=95c4ed43f7644dc8a05175665cd04b7a"
+    # # 95c4ed43f7644dc8a05175665cd04b7a - api key
+    # r = requests.get(top_stories).json()
+    # top = []
+    # for i in r['articles']:
+    #     top_story = {
+    #         'image': i['urlToImage'],
+    #         'title': i['title']
+    #     }
+    #     top.append(top_story)
+    # context_of_top_stories = {'top_stories': top}
+    # return render(request, "index.html", context_of_top_stories)
+    return render(request, "index.html")
 
 
 def auth_view(request, *args, **kwargs):
