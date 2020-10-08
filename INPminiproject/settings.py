cb10f7bf42c466e65a8612d0fbc0cc80b8410c57
reuben21 <reuben211999@icloud.com>
2020-10-09 00:18:22 +0530

    Updated Files

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '*%^bonl#^w5&ndeq)$tweo#*738@_um!g9iweffg)6v$1k#lj1'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,17 +74,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'INPminiproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'theJournalist',
+        'USERNAME': 'root',
+        'PASSWORD': 'Reuben@21',
+        'PORT': 3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -119,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth import authenticate
 # Create your views here.
 from authentication.form import PersonalDetails
+from django.contrib.auth.models import auth
+from django.db import connection
 
 
 # Create your views here.
-def auth(request):
+# def my_custom_sql(self):
 
+
+def auth(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if username != "" and password != "":
+        print(username, password)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT email_id,password FROM authentication_personaldetails WHERE email_id = %s",
+                           [username])
+            row = cursor.fetchone()
+            print(row)
+    # else:
     Registerform = PersonalDetails()
     if request.method == 'POST':
 
         form = PersonalDetails(request.POST)
-
         if form.is_valid():
             form.save()
         redirect('auth/')
