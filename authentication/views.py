63ff374e01a894577a46d1c7d446359df3f964c1
reuben21 <reuben211999@icloud.com>
2020-10-10 14:25:02 +0530

    Updated Code

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from django.db import connection
-
+from django.contrib.sessions.models import Session
 
 # Create your views here.
 # def my_custom_sql(self):
@@ -20,9 +20,9 @@ def auth(request):
     register_emailId = request.POST.get('reg_emailId')
     register_password = request.POST.get('reg_password')
     register_date = request.POST.get('dateofbirth')
-    auth_details = [register_firstname, register_lastname, register_emailId, register_password, register_date]
     if request.method == 'POST':
         if register_emailId != "" and register_firstname != "" and register_lastname != "" and register_password != "" and register_date != "":
+            hashed_password = generate_password_hash(register_password, method="sha256")
             status = email_id_status(register_emailId)
             if status == 'EMAIL ID TAKEN':
                 messages.info(request, "Email Id is Already Registered")
@@ -30,7 +30,7 @@ def auth(request):
                 with connection.cursor() as cursor:
                     print()
                     cursor.callproc('insert_personal_details',
-                                    [register_firstname, register_lastname, register_emailId, register_password,
+                                    [register_firstname, register_lastname, register_emailId, hashed_password,
                                      register_date])
                     context = {"registration_success": True}
                     return render(request, 'login.html', context)
@@ -46,7 +46,18 @@ def Login(request):
             cursor.execute("SELECT email_id,password FROM authentication_personaldetails WHERE email_id = %s",
                            [username])
             row = cursor.fetchone()
-
-            if row == None:
-                print("Invalid Credentials")
+            print(row)
+            if row is None:
+                messages.info(request, "Invalid Credentials")
+                context = {"registration_success": False}
+                return render(request, 'Login.html', context)
+            else:
+                if check_password_hash(row[1], password):
+                    messages.info(request, "Valid Credentials")
+                    context = {"registration_success": False}
+                    return render(request, 'Login.html', context)
+                else:
+                    messages.info(request, "Invalid Credentials")
+                    context = {"registration_success": False}
+                    return render(request, 'Login.html', context)
     return render(request, 'Login.html')
