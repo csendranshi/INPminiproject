5b8c850faf9f215a8cbb628674aa4c63e7e032b1
reuben21 <reuben211999@icloud.com>
2020-10-11 21:29:05 +0530

    Updated Files

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'INPminiproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'news_database',
+        'NAME': 'thejournalist',
         'USER': 'root',
         'PASSWORD': 'Reuben@21',
         'PORT': '3306',
-        'HOST':'127.0.0.1'
+        'HOST': '127.0.0.1'
 
     }
 }
