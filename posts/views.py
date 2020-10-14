77070d8c9799c8963448334f4730d365207d1af2
reuben21 <reuben211999@icloud.com>
2020-10-14 16:11:15 +0530

    Updated Code

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
+
+
 # Create your views here.
 def posts_view(request, *args, **kwargs):
-    return render(request, "posts.html", {})
+    if request.session.has_key('logged_in'):
+        print(request.session.has_key('logged_in'))
+        dict_of_user_details = {
+            'admin_access': request.session['admin_access'],
+            'journal_access': request.session['journal_access'],
+            'logged_in': request.session['logged_in'],
+            'first_name': request.session['first_name'],
+            'last_name': request.session['last_name'],
+            'email_id': request.session['email_id'],
+            'suscriber_access': request.session['suscriber_priority']
+
+        }
+        context_of_top_stories = {'user': dict_of_user_details}
+        return render(request, "posts.html", context_of_top_stories)
+    return redirect(request, '/')
