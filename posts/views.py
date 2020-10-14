9391c713f30b9469f9f9e2b2339d95f809898a28
anshika <csendranshi@gmail.com>
2020-10-14 22:56:31 +0530

    posts updated

--- posts/views.py
+++ posts/views.py
@@ -4,18 +4,6 @@ import requests
 
 # Create your views here.
 def posts_view(request, *args, **kwargs):
-    if request.session.has_key('logged_in'):
-        print(request.session.has_key('logged_in'))
-        dict_of_user_details = {
-            'admin_access': request.session['admin_access'],
-            'journal_access': request.session['journal_access'],
-            'logged_in': request.session['logged_in'],
-            'first_name': request.session['first_name'],
-            'last_name': request.session['last_name'],
-            'email_id': request.session['email_id'],
-            'suscriber_access': request.session['suscriber_priority']
 
-        }
-        context_of_top_stories = {'user': dict_of_user_details}
-        return render(request, "posts.html", context_of_top_stories)
-    return redirect(request, '/')
+        # return render(request, "posts.html", context_of_top_stories)
+    return render(request, 'posts.html',{})
