822fbcfdd514056eca9c0b2c4b2b46859215783a
reuben21 <reuben211999@icloud.com>
2020-10-16 06:10:30 +0530

    updated Code

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -127,9 +127,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
