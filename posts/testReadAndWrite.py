579d6aa51661008775e58d798d4d80c9581361e3
reuben21 <reuben211999@icloud.com>
2020-10-24 23:12:10 +0530

    Updated Code:

---
+++ posts/testReadAndWrite.py
@@ -0,0 +1,6 @@
+
+
+
+#open and read the file after the appending:
+f = open("unique_id.txt", "r")
+print(f.read())
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render, redirect
 import requests
 from django.db import connection
+import random
+
+def cell_list(prefix, get_range):
+    list_cell_string = []
+    for i in get_range:
+        cell_string = prefix + "-cell-"
+        cell_string += str(i)
+        list_cell_string.append(cell_string)
+    return list_cell_string
+
 
 def posts_view(request, *args, **kwargs):
+    latest_cell = cell_list('latest', range(1, 15))
+    health_cell = cell_list('health', range(1, 10))
+    world_cell = cell_list('world', range(1, 11))
+    business_cell = cell_list('business', range(1, 9))
+    tech_cell= cell_list('technology',range(1,11))
+    edu_cell = cell_list('education', range(1, 9))
+    india_cell = cell_list('india', range(1, 11))
+    dictionary_of_section_as_per_category = {
+        'latest': latest_cell,
+        'health': health_cell,
+        'world': world_cell,
+        'business': business_cell,
+        'technology':tech_cell,
+        'education':edu_cell,
+        'india':india_cell
+    }
+
     if request.session.has_key('logged_in'):
         dict_of_user_details = {
             'admin_access': request.session['admin_access'],
@@ -23,18 +50,22 @@ def posts_view(request, *args, **kwargs):
             article_category = request.POST.get('category')
             article_section = request.POST.get('section')
             article_file_picture = request.POST.get('finalPictureValue')
-            print(article_image_link,
-                  article_title,
-                  article_category,
-                  article_section,
-                  article_content)
 
+            n = random.randint(10000, 999999)
+            f = open("unique_id.txt", "a")
+            f.write(str(n) + "\n")
+            f.close()
             with connection.cursor() as cursor:
-                print()
-                cursor.execute('CALL news_database.latest_top_stories(%s,%s,%s,%s,%s,%s)',
+                print(article_image_link,
+                      article_title,
+                      article_category,
+                      article_section,
+                      article_content)
+                cursor.execute('CALL news_database.latest_top_stories(%s,%s,%s,%s,%s,%s,%s,%s)',
                                [article_title, article_file_picture, article_image_link, article_content,
-                                dict_of_user_details['email_id'], article_category])
+                                dict_of_user_details['email_id'], article_category,article_section,n])
                 print("clean")
-        context_of_top_stories = {'user': dict_of_user_details}
+        context_of_top_stories = {'user': dict_of_user_details, 'cell_list': dictionary_of_section_as_per_category}
         return render(request, 'posts.html', context_of_top_stories)
-    return render(request, 'posts.html', {})
+    context = {'cell_list': dictionary_of_section_as_per_category}
+    return render(request, 'posts.html', context)
