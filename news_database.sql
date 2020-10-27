3bf67160c431456f382bcac318b38c6cbd5a5f33
reuben21 <reuben211999@icloud.com>
2020-10-27 17:30:37 +0530

    Updated Code

--- news_database.sql
+++ news_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2020 at 06:30 PM
+-- Generation Time: Oct 27, 2020 at 12:59 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
