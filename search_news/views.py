910684e7bff4886a7d50643eb47078b3faa4cb0d
reuben21 <reuben211999@icloud.com>
2020-11-05 00:27:41 +0530

    Updated Code

--- search_news/views.py
+++ search_news/views.py
@@ -1,3 +1,6 @@
+import time
+
+from django.db import connection
 from django.shortcuts import render
 
 
@@ -16,6 +19,26 @@ def search_view(request, *args, **kwargs):
             'profile_picture': request.session['profile_picture']
 
         }
-        context_of_top_stories = {'user': dict_of_user_details}
-        return render(request, "search.html", context_of_top_stories)
+        if request.method == 'POST':
+            search_text = request.POST.get('search_text')
+            print("The Search Text Entered is: ", search_text)
+            with connection.cursor() as cursor:
+                cursor.execute('CALL news_database.search_news(%s)',
+                               [search_text])
+                rows = cursor.fetchall()
+                print(rows)
+                list_of_stories = []
+                for row in rows:
+                    stories_dict = {
+                        'section': row[4],
+                        'title': row[5],
+                        'image': row[1],
+                        'image_link': row[2],
+                        'category': row[3],
+                        'news_unique_id': row[0]
+                    }
+                    list_of_stories.append(stories_dict)
+
+            context_of_top_stories = {'user': dict_of_user_details, 'search_stories': list_of_stories}
+            return render(request, "search.html", context_of_top_stories)
     return render(request, "search.html", {})
