48de547b8524f3d50e4428653d216ebc679aed10
reuben21 <reuben211999@icloud.com>
2020-11-23 22:21:46 +0530

    Project

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -151,7 +151,9 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.getcwd(), "static")
-STATICFILES_DIRS = ( os.path.abspath(__file__)+'/..'+'/static', )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn INPminiproject.wsgi
