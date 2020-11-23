72910479224bf82272ccd111ddcc8c4d1d2ba527
reuben21 <reuben211999@icloud.com>
2020-11-24 00:21:55 +0530

    Project

--- Page404/views.py
+++ Page404/views.py
@@ -4,8 +4,8 @@ from django.template import RequestContext
 
 # Create your views here.
 def handler404(request, *args, **argv):
-    return render(request, "ErrorPage.html", {})
+    return render(request, "ErrorPage.html")
 
 
 def handler500(request, *args, **argv):
-    return render(request, "ErrorPage.html", {})
+    return render(request, "ErrorPage.html")
