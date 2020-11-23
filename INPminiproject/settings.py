0bf3b6b3d3b1d29a563e4f3d3743905e7f18ee75
reuben21 <reuben211999@icloud.com>
2020-11-23 22:24:54 +0530

    Project

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.getcwd(), "static")
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
