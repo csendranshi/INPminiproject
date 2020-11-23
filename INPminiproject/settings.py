6768acfa6b93f3df557f2f82a9653fa4b01edb1b
reuben21 <reuben211999@icloud.com>
2020-11-23 22:28:56 +0530

    Project

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -151,9 +151,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
