36d5b98a29a4f5d2b876ec159025e709f0f07e04
reuben21 <reuben211999@icloud.com>
2020-11-23 22:14:00 +0530

    Project

--- INPminiproject/settings.py
+++ INPminiproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*%^bonl#^w5&ndeq)$tweo#*738@_um!g9iweffg)6v$1k#lj1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -148,12 +148,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.getcwd(), "static")
+STATICFILES_DIRS = ( os.path.abspath(__file__)+'/..'+'/static', )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
