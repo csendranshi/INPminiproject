207d2196031d8c18a8ad19f062aab46c041bf093
reuben21 <reuben211999@icloud.com>
2020-11-25 13:58:01 +0530

    Updated Final Code

--- business/views.py
+++ business/views.py
@@ -3,12 +3,13 @@ from django.db import connection, Error
 
 # Create your views here.
 def business_view(request, *args, **kwargs):
+    list_of_business = []
     with connection.cursor() as cursor:
         try:
             cursor.execute('SELECT * from business_grid')
             rows = cursor.fetchall()
 
-            list_of_business = []
+
             for row in rows:
                 business_dict = {
                     'section': row[0],
@@ -19,6 +20,7 @@ def business_view(request, *args, **kwargs):
                     'news_unique_id':row[8]
                 }
                 list_of_business.append(business_dict)
+            print(list_of_business)
         except Error as err:
             return render(request, "ErrorPage.html", {"Error": err})
     if request.session.has_key('logged_in'):
@@ -37,4 +39,4 @@ def business_view(request, *args, **kwargs):
         context_of_top_stories = {'user': dict_of_user_details,'business_stories': list_of_business}
         return render(request, "Business.html", context_of_top_stories)
     context = {'business_stories': list_of_business}
-    return render(request, "Business.html", {})
+    return render(request, "Business.html", context)
